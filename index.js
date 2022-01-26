2ea6ecc21078807bd5b86fbe0959c72c7a35fe72
Joy Kirii <joykirii@gmail.com>
2022-01-26 11:33:17 +0300

    add event listerner to add btn

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict!';
+
+let addBtn = document.getElementById("add");
+
+function addTask(){
+
+  console.log('Task Added');
+
+}
+addBtn.addEventListener("click", function(){
+  console.log("button clicked");
+})
