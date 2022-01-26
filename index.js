7c93679c56bcdf1af36f4144d4ec47a422f90057
Joy Kirii <joykirii@gmail.com>
2022-01-26 11:41:35 +0300

    create for loop to log out items in the tasks array

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // Add task btn 
 const addBtn = document.getElementById("add");
 // tasks array 
-let tasks = [];
+let tasks = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 // input field 
 const toDo = document.getElementById("to-do");
 
@@ -13,8 +13,14 @@ function addTask(){
 
 }
 
-
+// When add button is clicked it renders out the tasks.
 addBtn.addEventListener("click", function(){
   tasks.push(toDo.value);
   console.log(tasks);
 });
+
+
+//For loop to log out items in the tasks array 
+for(let i = 0; i < tasks.length; i++){
+  console.log(tasks[i]);
+}
