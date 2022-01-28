ef36e42d8ebdf6e476322d06fb95af567acd8bdb
Joy Kirii <joykirii@gmail.com>
2022-01-28 20:07:58 +0300

    wrap code that renders out to-do-list in a function

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 'use strict';
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value);
   console.log(myLeads);
+  render();
 })
 
+function render() {
+  let listItems = " ";
+  for (let i = 0; i < myLeads.length; i++) {
+  listItems += "<li>" + myLeads[i] + "</li>" + "<hr>"; 
+  }
+
+  ulEl.innerHTML = listItems;
+};
 // Render the leads in the unordered list using ulEl.textContent
-for (let i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" + "<hr>";
-}
 
 
 
